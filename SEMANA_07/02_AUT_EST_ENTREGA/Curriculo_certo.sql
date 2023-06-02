e6ea08fa0e945dce873c4657b22f0355495f1189
LuanRM1 <luan.mello@sou.inteli.edu.br>
2023-06-02 08:55:38 -0300

    ponderada 7
    asdd

---
+++ SEMANA_07/02_AUT_EST_ENTREGA/Curriculo_certo.sql
@@ -0,0 +1,23 @@
+CREATE TABLE Pessoa ( 
+	Nome                 TEXT     ,
+	Id                   INTEGER NOT NULL  PRIMARY KEY  ,
+	CONSTRAINT unq_Pessoa_Id UNIQUE ( Id )
+ );
+
+CREATE TABLE Competencias ( 
+	Habilidades          INTEGER     ,
+	id                   INTEGER NOT NULL    ,
+	FOREIGN KEY ( id ) REFERENCES Pessoa( Id )  
+ );
+
+CREATE TABLE Experiencias ( 
+	Local                TEXT     ,
+	id                   TEXT NOT NULL    ,
+	FOREIGN KEY ( id ) REFERENCES Pessoa( Id )  
+ );
+
+CREATE TABLE Formação ( 
+	Instituição          TEXT     ,
+	Id                   INTEGER NOT NULL    ,
+	FOREIGN KEY ( Id ) REFERENCES Pessoa( Id )  
+ );
